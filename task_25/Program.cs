64450087c1fcf691a82a83a9eb1efc0d5da57214
Evgeny <buriy-anarchist@inbox.ru>
2022-07-14 18:18:56 +0300

    Add task25

---
+++ task_25/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int res = a;
+for (int i = 1; i < b; i++)
+{
+res = res * a;
+}
+
+Console.WriteLine(res);
